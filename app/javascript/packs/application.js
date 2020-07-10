bec989ab980461c65b240dd448ccfc88d973f74a
Koten <koten0224@gmail.com>
2020-07-10 16:26:53 +0800

    webpacker ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,11 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+import "controllers";
 
-console.log('Hello World from Webpacker')
+window.$ = $;
+
+require("popper.js");
+

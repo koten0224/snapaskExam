ae3c131d30331459d5fbda24e3ce2255c6bb1ecd
Koten <koten0224@gmail.com>
2020-07-12 20:00:27 +0800

    tutorial view crud complete

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,2 +1,15 @@
 class Tutorial < ApplicationRecord
+  belongs_to :user
+  
+  enum catagory: {
+    "lalala" => 0,
+    "yoyoyo" => 1,
+    "hohoho" => 2
+  }
+
+  enum price_type: {
+    "TWD" => 0,
+    "USD" => 1
+  }
+  
 end

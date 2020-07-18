daedc53fc02cbbfb90ae2890c0ec08f43f543afc
Koten <koten0224@gmail.com>
2020-07-19 00:53:58 +0800

    refactor purchase api

--- app/controllers/api/v0/purchase.rb
+++ app/controllers/api/v0/purchase.rb
@@ -5,23 +5,16 @@ class Api::V0::Purchase < Grape::API
   get "/purchased_tutorials/:auth_token" do
     params do
       requires :auth_token, type: String, desc: "User authenticity token"
-      optional :category_id, type: Integer, desc: "Filted by category_id"
+      optional :category, type: Integer, desc: "Filted by category_id"
       optional :available, type: String, desc: "Give true or false"
     end
 
     result  = current_user.purchased_tutorials
-                          .joins(:tutorial)
-                          .includes(:tutorial)
-                          .includes(:transaction_records)
-                          .includes(:category)
 
-    if params.include? "category_id"
-      category_id = params["category_id"]
-      result = result.where("tutorials.category_id = ?", category_id)
-    end
-
-    if params["available"] == "true"
-      result = result.where("purchased_tutorials.deadline > ?", Time.now)
+    ["category", "available"].each do |attribute|
+      if params.include? attribute
+        result = result.send(attribute, params[attribute])
+      end
     end
 
     present result, with: Api::V0::Entities::Purchase

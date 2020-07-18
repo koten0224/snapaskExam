52711a8118503a677ebb8881a4ce2495dd785a7c
Koten <koten0224@gmail.com>
2020-07-18 17:13:06 +0800

    purchase api simplize

--- app/controllers/api/v0/entities/record.rb
+++ app/controllers/api/v0/entities/record.rb
@@ -3,5 +3,6 @@ class Api::V0::Entities::Record < Api::V0::Entities::Base
   expose :price
   expose :currency
   expose :expiration
+  expose :created_at
 end
 
--- app/controllers/api/v0/purchase.rb
+++ app/controllers/api/v0/purchase.rb
@@ -34,27 +34,13 @@ class Api::V0::Purchase < Grape::API
       requires :tutorial_id, type: Integer, desc: "The tutorial that user want to buy"
     end
     tutorial = Tutorial.find(params[:tutorial_id])
-    if tutorial.available?
-      purchased = current_user.purchased_tutorials.find_or_create_by(tutorial_id: params[:tutorial_id])
-      if purchased.deadline && purchased.deadline > Time.now
-        resp = { error: "Your Tutorial is still available, not allow to purchase again." }
-        present resp
-      else
-        purchased.deadline = Time.now + tutorial.expiration.days
-        purchased.save
-        current_user.transaction_records.create(
-          purchased_tutorial_id: purchased.id,
-          price: tutorial.price,
-          currency: tutorial.currency,
-          expiration: tutorial.expiration
-        )
-
-        present purchased, with: Api::V0::Entities::Purchase
-      end
+    transaction = current_user.buy.tutorial(tutorial)
+    if transaction.success
+      resp = { message: transaction.message }
     else
-      resp = { error: "This tutorial is not available." }
-      present resp
+      resp = { error: true, message: transaction.message }
     end
+    present resp
   end
 end
 

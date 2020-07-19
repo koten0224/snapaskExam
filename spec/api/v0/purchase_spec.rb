134fc570399e0adf3dc0e83f70957d7235582e67
Koten <koten0224@gmail.com>
2020-07-19 17:25:27 +0800

    purchased spec refactor and add two conditions

--- spec/api/v0/purchase_spec.rb
+++ spec/api/v0/purchase_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Purchase tutorial", type: :request do
 
+  def result
+    JSON.parse(response.body)
+  end
+
+  def post_to_buy_tutorial
+    post "/api/v0/purchased_tutorials", params: {
+      auth_token: @user.auth_token,
+      tutorial_id: @tutorial.id
+    }
+  end
+
   before(:each) do
     @teacher = create :teacher
     @user = create :user
@@ -27,7 +38,6 @@ RSpec.describe "Purchase tutorial", type: :request do
 
     it 'should return list length 10.' do
       get "/api/v0/purchased_tutorials/#{@user.auth_token}"
-      result = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(result.length).to eq(10)
     end
@@ -35,7 +45,6 @@ RSpec.describe "Purchase tutorial", type: :request do
     it 'should return list match that category.' do
       category = @categories.sample
       get "/api/v0/purchased_tutorials/#{@user.auth_token}?category=#{category.id}"
-      result = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(result.length).to eq(category.tutorials.count)
     end
@@ -45,7 +54,6 @@ RSpec.describe "Purchase tutorial", type: :request do
         purchased.update(deadline: 1.days.ago)
       end
       get "/api/v0/purchased_tutorials/#{@user.auth_token}?available=true"
-      result = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(result.length).to eq(7)
     end
@@ -55,13 +63,16 @@ RSpec.describe "Purchase tutorial", type: :request do
       purchased_tutorials = @user.purchased_tutorials.category(category.id)
       purchased_tutorials.sample.update(deadline: 1.days.ago)
       get "/api/v0/purchased_tutorials/#{@user.auth_token}?category=#{category.id}&available=true"
-      result = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(result.length).to eq(purchased_tutorials.length - 1)
     end
   end
 
   context 'POST /api/v0/purchased_tutorials' do
+
+    before(:each) do
+      @tutorial = @tutorials.sample
+    end
 
     it 'should raise error.' do
       expect do
@@ -72,39 +83,45 @@ RSpec.describe "Purchase tutorial", type: :request do
     end
 
     it 'should be success.' do
-      tutorial = @tutorials.sample
-      post "/api/v0/purchased_tutorials", params: {
-        auth_token: @user.auth_token,
-        tutorial_id: tutorial.id
-      }
-      result = JSON.parse(response.body)
+      post_to_buy_tutorial
       expect(response.status).to eq 201
       expect(result).to include "message"
     end
 
     it 'should not buy twice.' do
-      tutorial = @tutorials.sample
-      2.times do
-        post "/api/v0/purchased_tutorials", params: {
-          auth_token: @user.auth_token,
-          tutorial_id: tutorial.id
-        }
-      end
-      result = JSON.parse(response.body)
+      2.times { post_to_buy_tutorial }
       expect(response.status).to eq 400
       expect(result).to include "message"
     end
 
     it 'should not buy if tutorial is not available.' do
-      tutorial = @tutorials.sample
-      tutorial.update(available: false)
-      post "/api/v0/purchased_tutorials", params: {
-        auth_token: @user.auth_token,
-        tutorial_id: tutorial.id
-      }
-      result = JSON.parse(response.body)
+      @tutorial.update(available: false)
+      post_to_buy_tutorial
       expect(response.status).to eq 400
       expect(result).to include "message"
+    end
+
+    it 'should be success if user purchased is expired.' do
+      @user.buy.tutorial(@tutorial)
+      purchased = @user.purchased_tutorials.find_by(tutorial_id: @tutorial.id)
+      purchased.update(deadline: 1.days.ago)
+      post_to_buy_tutorial
+      expect(response.status).to eq 201
+      expect(result).to include "message"
+      expect(@user.transaction_records.count).to eq(2)
+      expect(@user.purchased_tutorials.count).to eq(1)
+    end
+
+    it 'should not buy when tutorial is not available even though user purchased is expired.' do
+      @user.buy.tutorial(@tutorial)
+      purchased = @user.purchased_tutorials.find_by(tutorial_id: @tutorial.id)
+      purchased.update(deadline: 1.days.ago)
+      @tutorial.update(available: false)
+      post_to_buy_tutorial
+      expect(response.status).to eq 400
+      expect(result).to include "message"
+      expect(@user.transaction_records.count).to eq(1)
+      expect(@user.purchased_tutorials.count).to eq(1)
     end
   end
 end
